cdfb20de046cdf1e7df7708f9def3c3fd73b819d
Brendan StJohn <1828650+stjohnb@users.noreply.github.com>
2025-07-07 21:08:22 +0100

    Regenerate test target

---
+++ TempoStatusBarAppTests/TempoStatusBarAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TempoStatusBarAppTests.swift
+//  TempoStatusBarAppTests
+//
+//  Created by Brendan StJohn on 07/07/2025.
+//
+
+import Testing
+
+struct TempoStatusBarAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
